056e895b285bd385d5803e63469cd277ce4bfbf2
Alesia <alesiavaronich@gmail.com>
2022-07-31 23:00:41 +0300

    added tests: drag and drop within an iFrame

--- src/test/java/pages/pagefactorypages/DragAndDropFactoryPage.java
+++ src/test/java/pages/pagefactorypages/DragAndDropFactoryPage.java
@@ -1,12 +1,39 @@
 package pages.pagefactorypages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 
 public class DragAndDropFactoryPage extends BaseFactoryPage{
 
+    @FindBy(xpath = "//iframe[@class='demo-frame']")
+    private WebElement iFrame;
 
+    @FindBy(xpath = "//div[@id='draggable']")
+    private WebElement source;
+
+    @FindBy(xpath = "//div[@id='droppable']")
+    private WebElement target;
+
+    @FindBy(xpath = "//div[@id='droppable']/p")
+    private WebElement droppedText;
 
     public DragAndDropFactoryPage(WebDriver driver) {
         super(driver);
     }
+
+    public void switchToFrame() {
+        driver.switchTo().frame(iFrame);
+    }
+
+    public void dragAndDrop() {
+        Actions actions = new Actions(driver);
+        actions.clickAndHold(source).moveToElement(target).release().build().perform();
+    }
+
+    public String getDroppedText() {
+        return droppedText.getText();
+    }
+
 }
--- src/test/java/tests/DragAndDropTest.java
+++ src/test/java/tests/DragAndDropTest.java
@@ -1,7 +1,10 @@
 package tests;
 
 import constants.Urls;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.pagefactorypages.DragAndDropFactoryPage;
 
 public class DragAndDropTest extends BaseTest{
 
@@ -10,4 +13,13 @@ public class DragAndDropTest extends BaseTest{
         driver.get(Urls.HEROKUAPP_DRAGANDDROP_URL);
     }
 
+    @Test
+    public void dragAndDrop() {
+        DragAndDropFactoryPage dragAndDrop = new DragAndDropFactoryPage(driver);
+        dragAndDrop.switchToFrame();
+        dragAndDrop.dragAndDrop();
+        String actualText = dragAndDrop.getDroppedText();
+        String expectedText = "Dropped!";
+        Assert.assertEquals(actualText, expectedText, "No text or incorrect text message is displayed.");
+    }
 }
